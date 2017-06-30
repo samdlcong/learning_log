f6ba212ccf274018c5b33a01b3998ce3331e6909
samdlcong <samnew@126.com>
2017-06-30 13:30:19 +0800

    add create topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from .models import Topic
+from .forms import TopicForm
 # Create your views here.
 def index(request):
     """学习笔记的主页"""
@@ -16,4 +20,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
-    
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        #未提交数据：创建一个新表单
+        form = TopicForm()
+    else:
+        #未提交数据：创建一个新表单
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form':form}
+    return render(request, 'learning_logs/new_topic.html',context)
