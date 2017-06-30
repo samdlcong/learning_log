df630c6008d89cc93071c083c330934d42d25cfc
samdlcong <samnew@126.com>
2017-06-30 17:25:38 +0800

    add module users

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+"""为应用程序users定义URL模式"""
+from django.conf.urls import url
+from django.contrib.auth.views import login 
+
+from . import views
+
+urlpatterns = [
+    #登录页面
+    url(r'^login/$',login, {'template_name': 'users/login.html'}, name='login'),
+]
