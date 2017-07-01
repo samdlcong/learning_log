28e03db63c1a89baa9a934651549a01a48bc116b
samdlcong <samnew@126.com>
2017-07-01 14:01:34 +0800

    add user register

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     #登录页面
     url(r'^login/$',login, {'template_name': 'users/login.html'}, name='login'),
+    url(r'^logout/$',views.logout_view, name='logout'),
+    url(r'^register/$',views.register, name='register'),
 ]
